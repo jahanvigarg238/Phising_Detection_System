6c5d671c0ca5d1a622cc94065e5f607a45f79cd1
Jahanvi Garg <jahanvigarg238@gmail.com>
2026-05-03 15:53:04 +0530

    Final Commit

---
+++ app.py
@@ -0,0 +1,88 @@
+import sys
+import os
+
+import certifi
+ca = certifi.where()
+
+from dotenv import load_dotenv
+load_dotenv()
+mongo_db_url = os.getenv("MONGODB_URL_KEY")
+print(mongo_db_url)
+import pymongo
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+from networksecurity.pipeline.training_pipeline import TrainingPipeline
+
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, File, UploadFile,Request
+from uvicorn import run as app_run
+from fastapi.responses import Response
+from starlette.responses import RedirectResponse
+import pandas as pd
+
+from networksecurity.utils.main_utils.utils import load_object
+
+from networksecurity.utils.ml_utils.model.estimator import NetworkModel
+
+
+client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+
+from networksecurity.constant.training_pipeline import DATA_INGESTION_COLLECTION_NAME
+from networksecurity.constant.training_pipeline import DATA_INGESTION_DATABASE_NAME
+
+database = client[DATA_INGESTION_DATABASE_NAME]
+collection = database[DATA_INGESTION_COLLECTION_NAME]
+
+app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+from fastapi.templating import Jinja2Templates
+templates = Jinja2Templates(directory="./templates")
+
+@app.get("/", tags=["authentication"]) # This is the root endpoint of the API, which redirects to the documentation page.
+async def index():
+    return RedirectResponse(url="/docs")
+
+@app.get("/train") # This endpoint is used to trigger the training pipeline. When a GET request is made to this endpoint, it creates an instance of the TrainingPipeline class and calls the run_pipeline method to start the training process. If the training is successful, it returns a response indicating that the training was successful. If there is an exception during the training process, it raises a NetworkSecurityException with the error message and the sys module.
+async def train_route():
+    try:
+        train_pipeline=TrainingPipeline()
+        train_pipeline.run_pipeline()
+        return Response("Training is successful")
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+    
+@app.post("/predict")
+async def predict_route(request: Request,file: UploadFile = File(...)):
+    try:
+        df=pd.read_csv(file.file)
+        #print(df)
+        preprocesor=load_object("final_model/preprocessor.pkl")
+        final_model=load_object("final_model/model.pkl")
+        network_model = NetworkModel(preprocessor=preprocesor,model=final_model)
+        print(df.iloc[0])
+        y_pred = network_model.predict(df)
+        print(y_pred)
+        df['predicted_column'] = y_pred
+        print(df['predicted_column'])
+        #df['predicted_column'].replace(-1, 0)
+        #return df.to_json()
+        df.to_csv('prediction_output/output.csv')
+        table_html = df.to_html(classes='table table-striped')
+        #print(table_html)
+        return templates.TemplateResponse("table.html", {"request": request, "table": table_html})
+        
+    except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+    
+if __name__=="__main__":
+    app_run(app,host="localhost",port=8000)
